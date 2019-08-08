5d32dddff56d8d596b0f580358f04eaf453b08b0
preedus <preedus@gmail.com>
2019-08-08 17:21:09 +0300

    toolchain push

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,23 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 var Cloudant = require('@cloudant/cloudant');
 
+
+const port = process.env.PORT || 4000;
 const cloudant = Cloudant({
-    url:'...',
+    url:'https://fed93468-ff15-46e8-94a5-e16c1392397b-bluemix.cloudantnosqldb.appdomain.cloud',
     plugins:{
         iamauth:{
-            iamApiKey:'...'
+            iamApiKey:'JcGrhFRmRxeTnpFz2Dse4UJrBUhrQulqlPzuc3GIo3R4',
         }
     }
+}, async(err, client) => {
+    // called after db connection is ready
+    const dbList = await client.db.list();
+    const dbExists = dbList.indexOf('db_test') !== -1;
+    if(!dbExists){
+        client.db.create('db_test')
+    }
+
 });
 
 app.use(cors());
@@ -21,10 +31,12 @@ let todos = [];
 app.get('/',(req,res) => {
     res.send('Hello from server')
 })
-app.post('/add',(req,res) => {
+app.post('/add',async (req,res) => {
     const todo = req.body.todo;
     todos.push(todo);
     console.log('\ntodos',todos)
+    await cloudant.db.use('db_test').insert(todo);
+
     res.send({succes:true})
 })
 app.post('/delete/:id',(req,res) => {
@@ -45,11 +57,12 @@ app.post('/checked/:id',(req,res) => {
     console.log('\ntodos',todos)
     res.send({success:true})
 })
-app.get('/getTodos',(req,res) => {
+app.get('/getTodos', async(req,res) => {
+    // const dbTodos = await cloudant.db.use('db_test').find({selector:{}});
     res.send({todos})
   
 })
 
-app.listen(4000, (req, res) => {
+app.listen(port, (req, res) => {
     console.log('Server is running')
 })
